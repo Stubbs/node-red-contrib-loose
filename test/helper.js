d7c38553119df66d02e04dc56c9f1165f04c9cf6
Stuart Grimshaw <stuart.grimshaw@wandisco.com>
2015-12-17 00:47:21 +0000

    Added travis-ci config & node-red dev dependancy

--- test/helper.js
+++ test/helper.js
@@ -16,7 +16,7 @@
 
 var path = require('path');
 
-process.env.NODE_RED_HOME = process.env.NODE_RED_HOME || path.resolve(__dirname+"/../../node-red");
+process.env.NODE_RED_HOME = process.env.NODE_RED_HOME || path.resolve(__dirname+"/../node_modules/node-red");
 //console.log("HOME",process.env.NODE_RED_HOME);
 var helper = require(path.join(process.env.NODE_RED_HOME, 'test', 'nodes', 'helper.js'));
 
